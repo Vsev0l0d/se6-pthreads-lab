14c25dd92c61f8b0d8864edac141f5b034c1a8cc
Vsev0l0d <vsevolod628@gmail.com>
2022-04-14 21:04:48 +0300

    few renames

--- producer_consumer.cpp
+++ producer_consumer.cpp
@@ -7,9 +7,9 @@ typedef struct {
   const int sleep_millisecond_limit;
   const bool is_debug;
   const int* number;
-} pthrData;
+} consumerData;
 
-pthread_cond_t cond;
+pthread_cond_t cond_produced;
 pthread_cond_t cond_processed;
 pthread_mutex_t mutex;
 
@@ -28,7 +28,7 @@ void* producer_routine(void* arg) {
   while (scanf("%d", number) == 1) {
     pthread_mutex_lock(&mutex);
     printf("%d\n", *number);
-    pthread_cond_signal(&cond);
+    pthread_cond_signal(&cond_produced);
     pthread_cond_wait(&cond_processed, &mutex);
     pthread_mutex_unlock(&mutex);
   }
@@ -38,12 +38,12 @@ void* producer_routine(void* arg) {
 void* consumer_routine(void* arg) {
   // for every update issued by producer, read the value and add to sum
   // return pointer to result (for particular consumer)
-  pthrData* data = (pthrData*)arg;
+  consumerData* data = (consumerData*)arg;
   int* psum = (int*)malloc(sizeof(int));
   *psum = 0;
 
   pthread_mutex_lock(&mutex);
-  pthread_cond_wait(&cond, &mutex);
+  pthread_cond_wait(&cond_produced, &mutex);
   *psum += *(data->number);
   pthread_cond_signal(&cond_processed);
   pthread_mutex_unlock(&mutex);
@@ -67,27 +67,28 @@ int run_threads(int N, int sleep_millisecond_limit, bool is_debug = false) {
   // return aggregated sum of values
   int aggregated_sum = 0, number = 0;
   pthread_mutex_init(&mutex, NULL);
-  pthread_cond_init(&cond, NULL);
+  pthread_cond_init(&cond_produced, NULL);
   pthread_cond_init(&cond_processed, NULL);
-  pthread_t* threads = (pthread_t*)malloc((N + 1) * sizeof(pthread_t));
-  pthrData threadData = {sleep_millisecond_limit, is_debug, &number};
+  pthread_t* consumers = (pthread_t*)malloc(N * sizeof(pthread_t));
+  consumerData consumersData = {sleep_millisecond_limit, is_debug, &number};
 
   for (int i = 0; i < N; i++) {
-    pthread_create(&(threads[i]), NULL, consumer_routine, &threadData);
+    pthread_create(&(consumers[i]), NULL, consumer_routine, &consumersData);
   }
-  pthread_create(&(threads[N]), NULL, producer_routine, &number);
-  pthread_join(threads[N], NULL);
+  pthread_t producer;
+  pthread_create(&producer, NULL, producer_routine, &number);
+  pthread_join(producer, NULL);
 
   for (int i = 0; i < N; i++) {
     int* psum;
-    pthread_join(threads[i], (void**)&psum);
+    pthread_join(consumers[i], (void**)&psum);
     aggregated_sum += *psum;
     free(psum);
   }
 
-  free(threads);
+  free(consumers);
   pthread_cond_destroy(&cond_processed);
-  pthread_cond_destroy(&cond);
+  pthread_cond_destroy(&cond_produced);
   pthread_mutex_destroy(&mutex);
   return aggregated_sum;
 }
