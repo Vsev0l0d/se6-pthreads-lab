40591457dfbbbded2d67214dc559dcc24a8e7874
Vsev0l0d <vsevolod628@gmail.com>
2022-04-15 19:42:57 +0300

    ...

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -4,7 +4,7 @@
 #include <sys/resource.h>
 
 TEST_CASE("./posix 3 3" * doctest::timeout(1)) {
-  struct rlimit limit = {2, 2};
+  struct rlimit limit = {1, 1};
   setrlimit(RLIMIT_CPU, &limit);
   FILE* fp = freopen("tests/input.txt", "r", stdin);
   CHECK(55 == run_threads(3, 3));
