14f7c86f5cf874604d8e2a7a91ace2596b6ada91
Vsev0l0d <vsevolod628@gmail.com>
2022-04-15 15:08:43 +0300

    test 1

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -2,4 +2,8 @@
 #include <doctest.h>
 #include <producer_consumer.h>
 
-TEST_CASE("just_example") { CHECK(4 == 4); }
+TEST_CASE("./posix 3 3" * doctest::timeout(0.5)) {
+    FILE* fp = freopen("tests/input.txt", "r", stdin);
+    CHECK(55 == run_threads(3, 3));
+    fclose(fp);
+}
